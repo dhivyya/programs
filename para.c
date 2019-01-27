8dd4c7ef5d30401b9e76b245e100482ecf31b96a
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-01-27 21:02:53 +0530

    para.c

---
+++ para.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+   char s[100];
+
+   int l,c=1,i;
+
+   scanf("%s,s");
+
+   l=strlen(s);
+
+   for(i=0;i<l;i++)
+
+   {
+
+       if(s[i]=='.')
+
+       c++;
+
+       printf("%d",c);
+
+       else
+
+       printf("%d",c);
+
+   }
+
+
+
+    return 0;
+
+}
+
+
+
