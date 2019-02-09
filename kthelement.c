727fa48bf7a0b3c199beba59092d621c4b39531c
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-09 23:16:38 +0530

    kthelement.c

---
+++ kthelement.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,k,a[20];
+clrscr();
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(i==k)
+{
+printf("%d",a[i]);
+break;
+}
+}
+getch();
+}
