f08ca553ea843da49d2b6fcbfa62859a404ffce1
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-15 16:28:26 +0530

    roman.c

---
+++ roman.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a<=20)
+        switch(a)
+        {
+            case 1:
+            {
+                printf("I");
+                break;
+            }
+            case 2:
+            {
+                printf("II");
+                break;
+            }case 3:
+            {
+                printf("III");
+                break;
+            }case 4:
+            {
+                printf("IV");
+                break;
+            }case 5:
+            {
+                printf("V");
+                break;
+            }case 6:
+            {
+                printf("VI");
+                break;
+            }case 7:
+            {
+                printf("VII");
+                break;
+            }case 8:
+            {
+                printf("VIII");
+                break;
+            }case 9:
+            {
+                printf("IX");
+                break;
+            }case 10:
+            {
+                printf("X");
+                break;
+            }case 11:
+            {
+                printf("XI");
+                break;
+            }case 12:
+            {
+                printf("XII");
+                break;
+            }case 13:
+            {
+                printf("XIII");
+                break;
+            }case 14:
+            {
+                printf("XIV");
+                break;
+            }
+            case 15:
+            {
+                printf("XV");
+                break;
+            }case 16:
+            {
+                printf("XVI");
+                break;
+            }case 17:
+            {
+                printf("XVII");
+                break;
+            }case 18:
+            {
+                printf("XVIII");
+                break;
+            }case 19:
+            {
+                printf("XIX");
+                break;
+            }
+            case 20:
+            {
+                printf("XX");
+                break;
+            }
+        
+    }
+    return 0;
+}
