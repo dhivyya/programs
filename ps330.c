a051625b954db785a211eec30d55891495889b53
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-22 23:54:04 +0530

    ps330.c

--- ps330.c
+++ ps330.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[200],b[200];
+    int l,i,k,n,count=0;
+    gets(a);
+    gets(b);
+    l=strlen(a);
+    n=strlen(b);
+    scanf("%d",&k);
+    if(l==n)
+    {
+        for(i=0;i<l;i++)
+        {
+            if(a[i]!=b[i])
+            {
+                count++;
+            }
+        }
+    }
+if(count==k)
+{
+    printf("yes");
+}
+else
+printf("no");
+    return 0;
+}
 
