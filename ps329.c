4b3dbd6c386f1932e539610376a4bd0af640d6e3
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-24 23:53:13 +0530

    Update ps329.c

--- ps329.c
+++ ps329.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int l,r,c=0,i,j;
-    scanf("%d %d",&l,&r);
-    for(i=l+1;i<r;i++)
+    int l,r,l=0,i,j;
+    scanf("%d ,&l);
+          scanf("%d",&r);
+                for(i=l+1;i<r;i++)
     {
         for(j=2;j<r;j++)
         {
             if((j*j)==i)
             {
-                c++;
+                l++;
             }
         }
     }
-    printf("%d",c);
+    printf("%d",l);
 
     return 0;
 }
