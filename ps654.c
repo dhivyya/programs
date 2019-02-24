1c13a793876a27a95e76f9ce7594e4730146d97f
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-24 14:11:05 +0530

    ps654.c

---
+++ ps654.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];
+    int i,l,c,f=1;
+    gets(a);
+    gets(b);
+l=strlen(a);
+c=strlen(b);
+if(l==c)
+{
+    for(i=0;i<l;i++)
+    {
+        if(a[i]==b[i])
+        {
+            f=0;
+        }
+        else
+        f=1;
+        break;
+    }
+}
+
+if(f==0)
+{
+    printf("yes");
+}
+else
+printf("no");
+    return 0;
+}
