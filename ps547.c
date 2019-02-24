cec0a854b207872f9de8b512c1e5c2c2981751aa
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-02-24 20:43:29 +0530

    ps547.c

---
+++ ps547.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if((a>180)||(b>180)||(c>180))
+    {
+        printf("no");
+    }
+    else
+    {
+        if(a+b+c==180)
+        {
+            printf("yes");
+        }
+        else
+        printf("no");
+    }
+}
