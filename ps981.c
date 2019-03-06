6349448ce8055a3e118a6ee0e6ba5e095fa64bc9
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-03-06 20:25:34 +0530

    ps981.c

---
+++ ps981.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[10],i,j,m=0,b;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            b=((a[i])&(a[j]));
+            if(b>m)
+            {
+                m=b;
+            }
+        }
+    }
+printf("%d",m);
+    return 0;
+}
