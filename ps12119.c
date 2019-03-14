550e6ce95888c2fb21973c2fcc843874e27680b6
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-03-15 00:36:20 +0530

    ps12119.c

---
+++ ps12119.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main() 
+{
+	char str[30];
+	scanf("%s",str);
+	int i,l,value,sum=0;
+	l=strlen(str);
+	int j=l-1;
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>='A' && str[i]<='F')
+		value=(str[i]-65)+10;
+	
+		else if(str[i]>='0' && str[i]<='9')
+			value=str[i]-48;
+		sum=sum+(value*(pow(16,j)));
+		j--;
+	}
+	printf("%d",sum);
+}
