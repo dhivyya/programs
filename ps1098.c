03ee33e3eb83e92304484fd181c8bcb4580e875b
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-03-15 00:58:16 +0530

    ps1098.c

---
+++ ps1098.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+    char a[100];
+    int i,l,c=0,k;
+    gets(a);
+    scanf("%d",&k);
+    l=strlen(a);
+    for(i=0;i<l;i++) 
+     if(a[i]>=0 && a[i]<=k)
+             c++;
+     if(l==c)
+        printf("yes");
+    else
+         printf("no");
+    
+}
