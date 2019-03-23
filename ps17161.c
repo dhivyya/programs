d0ace2aaf6639b60095ea089a3404e0fcda777f1
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-03-23 16:13:50 +0530

    ps17161.c

--- ps17161.c
+++ ps17161.c
@@ -2,17 +2,17 @@
 int main() 
 {
 
-char str[30];
+char s[30];
 	int n,i,j,flag=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
-		scanf("%s\n",str);
+gets(s);
 
 	for(i=0;i<n;i++)
 	{
-		for(j=0;str[j]!='\0';j++)
+		for(j=0;s[j]!='\0';j++)
 		{
-			if(str[j]=='a'||str[j]=='e'||str[j]=='i'||str[j]=='o'||str[j]=='u'||str[j]=='A'||str[j]=='E'||str[j]=='I'||str[j]=='O'||str[j]=='U')
+			if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U')
 			{
 				flag=1;
 				break;
