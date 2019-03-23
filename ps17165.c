d7a4015df22972f6bb94b91ec5cb5453755fcaba
dhivyya <46838606+dhivyya@users.noreply.github.com>
2019-03-23 16:27:54 +0530

    ps17165.c

---
+++ ps17165.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,n,k,f=0,c;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+   c=a[0];
+    for(i=0;i<n;i++)
+    {
+   if((a[i]>c)&&(a[i]>k))
+         {
+             printf("%d",a[i]);
+             break;
+         }
+         else
+           c=a[i];
+           }
+           return 0;
+   }
